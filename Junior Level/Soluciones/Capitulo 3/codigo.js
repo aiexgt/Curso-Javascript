cf4540ace357e80653055e155ab3a84a7fd6a93c
aiexgt <lima14florian14@gmail.com>
2022-01-25 21:53:29 -0600

    Problema C Capitulo 3

--- Junior Level/Soluciones/Capitulo 3/codigo.js
+++ Junior Level/Soluciones/Capitulo 3/codigo.js
@@ -76,3 +76,70 @@ document.write(`
     `
 )
 */
+
+//Problema C
+class App{
+    constructor(descargas,puntuacion,peso){
+        this.descargas = descargas;
+        this.puntuacion = puntuacion;
+        this.peso = peso;
+        this.iniciada = false;
+        this.instalada = false;
+    }
+    instalar(){
+        if(this.instalada == false){
+            this.instalada = true;
+            alert("App instalada correctamente");
+        }
+    }
+    desinstalar(){
+        if(this.instalada == true){
+            this.instalada = false;
+            alert("App desinstalada correctamente");
+        }
+    }
+    abrir(){
+        if(this.iniciada == false && this.instalada == true){
+            this.iniciada = true;
+            alert("App abierta");
+        }
+    }
+    cerrar(){
+        if(this.iniciada == true && this.instalada == true){
+            this.iniciada = false;
+            alert("App cerrada");
+        }
+    }
+    appInfo(){
+        return `
+        Descargas: <b> ${this.descargas} </b><br>
+        Puntuación: <b> ${this.puntuacion} </b><br>
+        Peso: <b> ${this.peso} </b><br>
+        `;
+    }
+}
+
+const app = new App("16,000","5 estrellas","900mb");
+const app2 = new App("1,000","4 estrellas","400mb");
+const app3 = new App("6,000","4.5 estrellas","100mb");
+const app4 = new App("23,000","4.8 estrellas","1gb");
+const app5 = new App("900","5 estrellas","250mb");
+const app6 = new App("17","3.7 estrellas","522mb");
+const app7 = new App("42,981","2.9 estrellas","723mb");
+
+document.write(`
+    ${app.appInfo()} <br>
+    ${app2.appInfo()} <br>
+    ${app3.appInfo()} <br>
+    ${app4.appInfo()} <br>
+    ${app5.appInfo()} <br>
+    ${app6.appInfo()} <br>
+    ${app7.appInfo()} <br>
+`);
+
+/*
+app.instalar();
+app.abrir();
+app.cerrar();
+app.desinstalar();
+*/
