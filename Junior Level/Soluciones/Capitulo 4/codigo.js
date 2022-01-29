e4f00fd2ceeacbe1b3f03f9f808e398a2823ea7a
aiexgt <lima14florian14@gmail.com>
2022-01-28 19:20:51 -0600

    Problema C Capitulo 4

--- Junior Level/Soluciones/Capitulo 4/codigo.js
+++ Junior Level/Soluciones/Capitulo 4/codigo.js
@@ -75,7 +75,7 @@ alert("Resultado: " + resultado);
 */
 
 //Problema B
-
+/*
 const obtenerInformacion = (materia)=>{
     let materias = {
         fisica: ["Lopez","Alex","Miguel","Rene","Daily","Cofla"],
@@ -123,3 +123,65 @@ mostrarInformacion("logica");
 
 document.write(cantidadClases("Cofla"));
 document.write(cantidadClases("Alex"));
+
+*/
+
+//Problema C
+let materias = {
+    fisica: ["Perez","pedro","pepito","cofla","maria"],
+    programacion: ["Dalto","pedro","juan","pepito"],
+    logica: ["Hernandez","pedro","juan","pepito","cofla","maria"],
+    quimica: ["Rodriguez","pedro","juan","pepito","cofla","maria"]
+}
+
+
+const inscribir = (alumno,materia)=>{
+personas = materias[materia];
+if (personas.length >= 21) {
+    document.write(`lo siento ${alumno}, las clases de ${materia} ya estan llenas<br><br>`);
+} else {
+    personas.push(alumno);
+    if (materia == "fisica") {
+        materias = {
+            fisica: personas,
+            programacion: materias['programacion'],
+            logica: materias['logica'],
+            quimica:materias['quimica']
+        }
+    }
+    else if (materia == "programacion") {
+        materias = {
+            fisica: materias['fisica'],
+            programacion: personas,
+            logica: materias['logica'],
+            quimica:materias['quimica']
+        }
+    }else if (materia == "logica") {
+        materias = {
+            fisica: materias['fisica'],
+            programacion: materia['programacion'],
+            logica: personas,
+            quimica:materias['quimica']
+        }
+    }else if (materia == "quimica") {
+        materias = {
+            fisica: materias['fisica'],
+            programacion: materia['programacion'],
+            logica: materias['logica'],
+            quimica: personas
+        }
+    }
+    document.write(`¡Felicidades ${alumno}! te has inscrito a ${materia} correctamente.<br><br>`);
+}
+}
+
+document.write(materias['fisica'] + "<br>")
+
+inscribir("pedrito","fisica");
+inscribir("jorge","fisica");
+inscribir("ramses","fisica");
+inscribir("pedrito","fisica");
+inscribir("jorge","fisica");
+inscribir("ramses","fisica");
+
+document.write("<br>" + materias['fisica'])
