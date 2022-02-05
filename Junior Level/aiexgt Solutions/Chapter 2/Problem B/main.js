f34778af94c5e1d5846747fd0c0f699557e769aa
aiexgt <lima14florian14@gmail.com>
2022-02-05 16:16:31 -0600

    Problem B - Chapter 2 Solved

--- Junior Level/aiexgt Solutions/Chapter 2/Problem B/main.js
+++ Junior Level/aiexgt Solutions/Chapter 2/Problem B/main.js
@@ -1,27 +1,30 @@
 'use strict'
 
-let register = document.querySelector("#problem-b"); 
+let register = document.querySelector("#contenedor")
 let student = [];
-let counterDays = 1;
-let documentFragment = document.createDocumentFragment();
+let day = 1;
 
+// Función para registrar estudiantes (cantidad y nombres)
 const registerStudent = ()=>{
-    let quantity = prompt("Cuantos alumnos desea registrar?",0);
-    for(let i=1;i<=quantity;i++){
-        student[i] = prompt("Nombre de alumno: ","");
+    let quantity = prompt("¿Cuantos alumnos desea registrar?",0);
+    for(let i=0;i<quantity;i++){
+        student.push([prompt("Nombre del alumno: ",)]);
+        student[i].push([]);
     }
 }
 
+// Función para imprimir el formulario
+let documentFragment = document.createDocumentFragment();
 const takeAttendance = (day)=>{
     let divRegister = `
     <h4 id="tittle-day">Día #${day}</h4>
     <form>
     `;
-    for(let i=1;i<student.length;i++){
+    for(let i in student){
         divRegister = divRegister + `
-            <label for="student${i}"><b>${student[i]}</b></label><br>
-            <input type="radio" id="student${i}" name="student${i}" value="p">Presente
-            <input type="radio" id="student${i}" name="student${i}" value="a">Ausente<br><br>
+            <label for="student${i}"><b>${student[i][0]}</b></label><br>
+            <input type="radio" class="student${i}p" name="student${i}" value="p">Presente
+            <input type="radio" class="student${i}a" name="student${i}" value="a">Ausente<br><br>
             `;
         };
     divRegister = divRegister + `
@@ -29,25 +32,53 @@ const takeAttendance = (day)=>{
     </form>
     `;
     let div = document.createElement("DIV");
+    div.classList.add(".flex-item")
     div.innerHTML = divRegister;
     documentFragment.appendChild(div);
     return documentFragment;
 }
 
-registerStudent();
-let salida = takeAttendance(counterDays);
-register.appendChild(salida);
-
-
-let button = document.querySelector("#enviar");
-button.addEventListener('click',function(){
-    for(let i=1;i<student.length;i++){
-        event.preventDefault();
-        let studentValue = document.querySelector(`input[name="student${i}"]:checked`);
-        student[i][counterDays] = studentValue.value;
-        console.log(student[i][counterDays]);
+//Funcion para imprimir resultados
+const printResult = () =>{
+    let divResult = `
+    <h2>Resultado</h2>
+    <div>
+    `;
+    for(let i in student){
+        let asistence = 1
+        for(let j of student[i][1]){
+            if(j == "p"){
+                asistence++;
+            }
+        }
+        divResult = divResult + `
+            <h4>Estudiante: ${student[i][0]}</h4>
+            Asistencias: ${asistence}</p>
+        `;
+        if(asistence > 18){
+            divResult = divResult + `<p style="color:green"><b>Aprobado</b></p>`;
+        }else{
+            divResult = divResult + `<p style="color:red"><b>Reprobado</b></p>`;
+        }
+        divResult = divResult + "<br><br>"
+        register.innerHTML = divResult;
     }
-    counterDays++;
-    let tittleDay = document.querySelector("#tittle-day");
-    tittleDay.innerHTML = `Día #${counterDays}`;
+}
+
+registerStudent();
+register.appendChild(takeAttendance(day));
+
+let buttonSend = document.querySelector("#enviar");
+buttonSend.addEventListener('click',function(){
+    event.preventDefault();
+    if(day < 30){    
+        for(let i in student){
+            student[i][1].push(document.querySelector(`input[name="student${i}"]:checked`).value);
+        }
+        day++;
+        document.querySelector("#tittle-day").innerHTML = `Día #${day}`;
+    }else{
+        printResult();
+    }
 });
+
