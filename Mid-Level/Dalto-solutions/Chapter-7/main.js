203e29b957f4c49cd219dc5d334c052f22a38c72
aiexgt <lima14florian14@gmail.com>
2022-02-20 17:28:50 -0600

    Chapter 7 - Problem A  Solved

---
+++ Mid-Level/Dalto-solutions/Chapter-7/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//Problem A
+/*
+let alto = window.screen.height;
+let ancho = window.screen.width;
+
+comprar = confirm(`El alto es: ${alto}, el ancho es: ${ancho}.
+¿Desea comprarla?
+`);
+
+if(comprar){
+    alert("Compra realizada");
+}else{
+    alert("Compra cancelada");
+}
+*/
